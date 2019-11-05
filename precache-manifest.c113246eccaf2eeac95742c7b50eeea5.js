47d9a44fb7ca65ebe00a7228b9fbe95f72919eaa
oliver rudoll <>
2019-11-05 19:12:36 +0100

    Updates

---
+++ precache-manifest.c113246eccaf2eeac95742c7b50eeea5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e479e002b0cad27f1f1d5ff0cf906875",
+    "url": "/GraphVizTechEARudoll/index.html"
+  },
+  {
+    "revision": "ae134911427f4a336a27",
+    "url": "/GraphVizTechEARudoll/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "c54356834bcabb3bdb8f",
+    "url": "/GraphVizTechEARudoll/static/css/main.c38a4bac.chunk.css"
+  },
+  {
+    "revision": "ae134911427f4a336a27",
+    "url": "/GraphVizTechEARudoll/static/js/2.b415e27d.chunk.js"
+  },
+  {
+    "revision": "c54356834bcabb3bdb8f",
+    "url": "/GraphVizTechEARudoll/static/js/main.88e06a29.chunk.js"
+  },
+  {
+    "revision": "aa3384269e6b88481b40",
+    "url": "/GraphVizTechEARudoll/static/js/runtime-main.61368249.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GraphVizTechEARudoll/precache-manifest.43dddb0e733cfba9552d8fdca3a91593.js"
+  "/GraphVizTechEARudoll/precache-manifest.c113246eccaf2eeac95742c7b50eeea5.js"
 );
 
 self.addEventListener('message', (event) => {
